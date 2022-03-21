a31ab9e544b918fcf28deb14035a62618a394ff3
kayla <kay.ann2000@gmail.com>
2022-03-21 14:53:40 -0600

    commit tree 4f62746cdbca951b87c82ad11574d1f13e0762e0

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+// // Your code goes here
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("The DOM has loaded");
+//    document.getElementById(p);
+// p.textContent = "This is really cool!"
+//   });
+  
+//   console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   );
+  let text = document.getElementById('text');
+  document.addEventListener("DOMContentLoaded", (event) => {
+  text.textContent = `This is really cool!`
+  })
